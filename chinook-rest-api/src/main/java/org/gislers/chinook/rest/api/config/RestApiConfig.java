ab5dc0890671a0a7efcc268c7868d99bf575c603
James Gisler <james.gisler@nike.com>
2015-09-15 12:11:23 -0700

    configuring objectMapper

--- chinook-rest-api/src/main/java/org/gislers/chinook/rest/api/config/RestApiConfig.java
+++ chinook-rest-api/src/main/java/org/gislers/chinook/rest/api/config/RestApiConfig.java
@@ -1,9 +1,16 @@
 package org.gislers.chinook.rest.api.config;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 /**
  * Created by:   jgisle
@@ -11,10 +18,25 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
  */
 @Configuration
 @ComponentScan("org.gislers.chinook")
-public class RestApiConfig {
+public class RestApiConfig extends WebMvcConfigurationSupport {
+
+    protected void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(converter());
+        addDefaultHttpMessageConverters(converters);
+    }
 
     @Bean
     public static PropertySourcesPlaceholderConfigurer applicationProperties() {
         return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    @Bean
+    MappingJackson2HttpMessageConverter converter() {
+        Jackson2ObjectMapperBuilder objectMapperBuilder = new Jackson2ObjectMapperBuilder();
+        objectMapperBuilder.serializationInclusion(JsonInclude.Include.NON_NULL);
+
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(objectMapperBuilder.build());
+        return converter;
     }
 }
